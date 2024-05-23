00474737e7563220dcccddcdb277d1cacf476634
Andrius Ašmanavičius <andrius.asmanavicius@gmail.com>
2024-05-23 15:52:39 +0300

    paskaita 1

--- paskaita_1/Program.cs
+++ paskaita_1/Program.cs
@@ -1,17 +1,43 @@
-﻿namespace paskaita1
+﻿namespace paskaita_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("    /\\   ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("   /  \\  ");
-            Console.WriteLine("  /    \\ ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(" /______\\");
-            Console.ForegroundColor= ConsoleColor.Gray;
+            
+            Console.Write("Iveskite skaiciu:");
+            string skaicius = Console.ReadLine();
+            int a = int.Parse(skaicius);
+            if (a > 100)
+            {
+                Console.WriteLine("skaicius didesnis uz 100");
+            }
+            else
+            {
+                Console.WriteLine("skaicius mazesnis uz 100");
+            }
+
+            int mypass = 11110000;
+            Console.Write("Iveskite slaptazodi:");
+            string slaptazodis = Console.ReadLine();
+            int b = int.Parse(slaptazodis);
+            bool raides = slaptazodis.Any(char.IsLetter);      
+            if (raides)
+            {
+                Console.WriteLine("Slaptazodis neteisingas.");
+            }
+            else
+            {
+                if (b == mypass)
+                {
+                    Console.WriteLine("Slaptazodis neteisingas");
+                }
+                else 
+                { 
+                    Console.WriteLine("Slaptazodis neteisingas"); 
+                }
+                
+            }
         }
     }
 }
---
+++ paskaita_3/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace paskaita_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("Kuria uzduoti norite atlikti (1,2,3?):");
+            string nrr = Console.ReadLine();
+            var nr = int.Parse(nrr);
+            switch (nr)
+            {
+                case 1:
+                    Console.WriteLine("Iveskite teksta:");
+                    string sk = Console.ReadLine();
+                    var skarr = sk.ToCharArray();
+                    skarr[0] = char.ToUpper(skarr[0]);
+                    Console.WriteLine(skarr);
+                    break;
+                case 2:
+                    Console.WriteLine("Iveskite teksta:");
+                    string text = Console.ReadLine();
+                    string res = text.Replace('2', 'g').Replace('4', 'b').Replace('6', '*').Replace('8', 'x').Replace("10", 'w'.ToString());
+                    Console.WriteLine(res);
+                    break;
+                case 3:
+                    Console.WriteLine("Iveskite 5 simboliu teksta:");
+                    string text5 = Console.ReadLine();
+                    char[] textarr = text5.ToCharArray();
+                    if (textarr.Length == 5)
+                    {
+                        Console.WriteLine("Kaip norite uzkoduoti pranesima, iveskite viena skaiciu:");
+                        string kodas = Console.ReadLine();
+                        int kodassk = int.Parse(kodas);
+                        if (kodas.Length != 1)
+                        {
+                            Console.WriteLine("Ivestas kodas neatitinka reikalavimu");
+                        }
+                        else
+                        {                         
+                            textarr[0] = (char)(textarr[0] + kodassk);
+                            textarr[1] = (char)(textarr[1] + kodassk);
+                            textarr[2] = (char)(textarr[2] + kodassk);
+                            textarr[3] = (char)(textarr[3] + kodassk);
+                            textarr[4] = (char)(textarr[4] + kodassk);
+
+                            console.WriteLine("Uzkoduotas tekstas:");
+                            Console.WriteLine(textarr);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ivestas tekstas neatitinka reikalavimu");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Tokios uzduoties nera");
+                    break;
+
+
+            }
+        }
+    }
+}
