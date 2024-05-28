f893402d9b5f0ad6d46641ec6df34248867047a0
Andrius Ašmanavičius <andrius.asmanavicius@gmail.com>
2024-05-28 16:31:22 +0300

    lesson 5

--- paskaita_5/Program.cs
+++ paskaita_5/Program.cs
@@ -1,10 +1,79 @@
-﻿namespace paskaita_5
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace paskaita_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // 1.Uzduotis
+            //string skaicius = "";
+            //double suma = 0;
+            //while (!(skaicius == "baigti"))
+            //{
+            //    Console.WriteLine("Iveskite skaicius:");
+            //    skaicius = Console.ReadLine();
+            //    double.TryParse(skaicius, out double skaiciusint);
+            //    suma = suma + skaiciusint; 
+            //    Console.WriteLine(suma);
+            //}
+            //Console.WriteLine($"galutine suma:" + suma);
+
+            // 2.Uzduotis
+            //string slaptazodis = "";
+            //while (!(slaptazodis == "test123"))
+            //{
+            //    Console.WriteLine("Iveskite slaptazodi:");
+            //    slaptazodis = Console.ReadLine();
+            //    if (!(slaptazodis == "test123"))
+            //    {
+            //        Console.WriteLine("SLAPTAZODIS NETEISINGAS");
+            //    }
+            //}
+            //Console.WriteLine("********slaptazodis teisingas********");
+
+            // 3.Uzduotis
+            
+            string password = "";
+            string email = "";
+            
+            Console.WriteLine("Iveskite slaptazodi:");
+            password = Console.ReadLine();
+            Console.WriteLine(IsPasswordValid(password));
+
+            Console.WriteLine("Iveskite email:");
+            email = Console.ReadLine();
+            Console.WriteLine(IsEmailValid(email));
+
+            Console.WriteLine("Iveskite suma doleriais:");
+            string dollars = Console.ReadLine();
+            double.TryParse(dollars, out double dollarsdouble);
+            Console.WriteLine($"Turite " + ConvertToEuros(dollarsdouble) + " eurus");
+
+        }
+        public static bool IsPasswordValid(string password)
+        {
+            if (password.Length > 8)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public static bool IsEmailValid(string email)
+        {
+            if (email.Contains("@") && email.Contains("."))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public static double ConvertToEuros(double dollars)
+        {   
+            double euro = dollars * 0.85;
+            return euro;
         }
     }
 }
