009b43ee07d48ea03f524048fb66aba2df189715
Andrius Ašmanavičius <andrius.asmanavicius@gmail.com>
2024-05-28 16:43:38 +0300

    lesson 5-1

--- paskaita_5/Program.cs
+++ paskaita_5/Program.cs
@@ -35,21 +35,25 @@ namespace paskaita_5
 
             // 3.Uzduotis
             
-            string password = "";
-            string email = "";
+            //string password = "";
+            //string email = "";
             
-            Console.WriteLine("Iveskite slaptazodi:");
-            password = Console.ReadLine();
-            Console.WriteLine(IsPasswordValid(password));
+            //Console.WriteLine("Iveskite slaptazodi:");
+            //password = Console.ReadLine();
+            //Console.WriteLine(IsPasswordValid(password));
 
-            Console.WriteLine("Iveskite email:");
-            email = Console.ReadLine();
-            Console.WriteLine(IsEmailValid(email));
+            //Console.WriteLine("Iveskite email:");
+            //email = Console.ReadLine();
+            //Console.WriteLine(IsEmailValid(email));
 
-            Console.WriteLine("Iveskite suma doleriais:");
-            string dollars = Console.ReadLine();
-            double.TryParse(dollars, out double dollarsdouble);
-            Console.WriteLine($"Turite " + ConvertToEuros(dollarsdouble) + " eurus");
+            //Console.WriteLine("Iveskite suma doleriais:");
+            //string dollars = Console.ReadLine();
+            //double.TryParse(dollars, out double dollarsdouble);
+            //Console.WriteLine($"Turite " + ConvertToEuros(dollarsdouble) + " eurus");
+
+            // 4.Uzduotis
+
+
 
         }
         public static bool IsPasswordValid(string password)
@@ -75,5 +79,29 @@ namespace paskaita_5
             double euro = dollars * 0.85;
             return euro;
         }
+
+        public static string GetInitials (string fistName,string lastName)
+        {
+            return fistName + " " + lastName;
+        }
+
+        public static double CalculateCylinderVolume (double radius, double height)
+        {
+            double turis = Math.PI * Math.Pow(radius, 2) * height;
+            return turis;
+        }
+
+        public static bool IsNumberEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+
+
+        
     }
 }
