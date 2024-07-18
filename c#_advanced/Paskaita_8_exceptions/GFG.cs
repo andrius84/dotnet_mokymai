5de1d64af26cd6d1a17970c159efd5d8eff2f5b2
Andrius Ašmanavičius <andrius.asmanavicius@gmail.com>
2024-07-18 22:02:33 +0300

    9 lesson extensions

---
+++ c#_advanced/Paskaita_8_exceptions/GFG.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paskaita_8_exceptions
+{
+    internal class GFG
+    {
+        public void Method2WithTryCatch()
+        {
+            try
+            { 
+                int[] arr = { 1, 2, 3, 4, 5 };
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                Console.WriteLine(arr[7]);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Method3WithTryCatch() 
+        {
+            try
+            {
+                int[] arr = { 19, 0, 75, 52 };
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i] / arr[i + 1]);
+                }
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ c#_advanced/Paskaita_9_extensions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Paskaita_9_extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+
+            Console.WriteLine($"Ar skaicius {number} teigimas: {number.IsPositiveOrNegative()}");
+
+            Console.WriteLine($"Ar skaicius {number} lyginis: {number.IsEven()}");
+
+            int number2 = 10;
+            Console.WriteLine($"Ar skaicius {number} didesnis uz {number2}: {number.IsNumberBiggerThan(number2)}");
+
+            string sentence = "Labas Rytas";
+            Console.WriteLine($"Ar sakinyje {sentence} yra tarpu: {sentence.IsThereSpace()}");
+
+            string fullname = "Vardenis Pav";
+            string yearOfBirth = "1990";
+            string domain = "com";
+            Console.WriteLine($"Email: {fullname.FormatEmail(yearOfBirth, domain)}");
+
+            List<string> words = new List<string> { "obuolys", "bananas", "kriause" };
+            string searchWord = "bananas";
+            Console.WriteLine($"Rasta: {words.FindAndReturnIfEqual("bananas")}");
+
+            List<string> words2 = new List<string> { "kriause", "bananas", "kriause", "slyva", "kriause", "obuolys", "kriause", "citrina", "kriause" };
+            List<string> everyOtherWord = words2.EveryOtherWord();
+            foreach (var word in everyOtherWord)
+            {
+                Console.Write($"{word} ");
+            }
+
+        }
+    }
+}
